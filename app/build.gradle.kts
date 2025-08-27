6ed1c80ec73cd3be5bb64bdfcb22dd53354a73ee
Developer From Jokela <developerfromjokela@gmail.com>
2025-08-28 00:33:25 +0300

    Implement Location Send to Car and sending SMS with ondevice provider

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.developerfromjokela.opencarwings"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         multiDexEnabled = true
